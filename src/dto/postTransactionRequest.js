1385575c655ef911577416d99a9ccfaf4baa360a
Atiqur Rehman <atiqur9911@gmail.com>
2022-07-31 19:05:59 +0200

    created dto for posting transactions

--- src/dto/postTransactionRequest.js
+++ src/dto/postTransactionRequest.js
@@ -1,23 +1,24 @@
 const { json } = require("body-parser")
 
-const postTransactionRequest = {  
+const postTransactionRequest = {
   id: '',
-  amount:{
-    value:'',
+  amount: {
+    value: '',
     unit: '',
     currency: '',
-    },
+  },
   type: '',
   status: '',
   meta_info: {
-    card_id:'',
-      merchant: {
-        id: '',
-        category_code:'',
-        country_code:'',
-        name:''
-      }
+    card_id: '',
+    merchant: {
+      id: '',
+      category_code: '',
+      country_code: '',
+      name: ''
     }
+  }
 }
+
 
 exports.Module = postTransactionRequest;
