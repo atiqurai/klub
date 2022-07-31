a018058fc4d82b2d370c3c29a80f98aad2b0802b
Atiqur Rehman <atiqur9911@gmail.com>
2022-07-31 02:07:22 +0200

    created controller for posting transactions

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const connection =  require("./database/connection");
+const transactionsRouter = require('./routers/transactions');
+
+const PORT = process.env.SERVER_PORT;
+const TRANSACTIONS_ROUTE = '/webhooks/transactions';
+
+const app  = express();
+app.use(TRANSACTIONS_ROUTE, transactionsRouter.transactions);
+//app.use("/webhooks/transactions",middleware,);
+app.listen(PORT, (req, res) => 
+    console.log("app is listening on port : " + PORT)
+);
--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { beginTransaction } = require('../database/connection');
 const router = express.Router();
 
-router.post((req, res, next) => {/*TODO : create a middleware*/
-
-
+router.post('/', (req, res, next) => {
+    res.send("working");
+    console.log("POST -START - TRANSACTIONS");
+    console.log("POST -END - TRANSACTIONS");
 });
 
-module.export = router;
+exports.transactions = router;
